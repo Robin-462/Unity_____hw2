ae13d4d2126621fbeb7475174c185b7e3fc5d62c
Robin-462 <ruochenzheng4@gmail.com>
2024-07-31 17:05:40 +0800

    Update LaserShooter.cs

--- Assets/Assets/HoverCraft/Prefabs/Bolt/Examples/LaserShooter.cs
+++ Assets/Assets/HoverCraft/Prefabs/Bolt/Examples/LaserShooter.cs
@@ -16,7 +16,7 @@ public class LaserShooter : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && enabled)
         {
             FireLaser();
         }
